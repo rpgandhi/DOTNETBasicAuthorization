9db9b8554066f1ea4165d2904dea521676ec08b9
Rakhee Gandhi and Rane Fields <rakhee+rane@>
2018-01-16 12:00:40 -0800

    Add authentication

---
+++ BasicAuthentication/Models/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicAuthentication.Models
+{
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+        public DbSet<Item> Items { get; set; }
+        
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(entity => {
+				entity.Property(m => m.Email).HasMaxLength(127);
+				entity.Property(m => m.NormalizedEmail).HasMaxLength(127);
+				entity.Property(m => m.UserName).HasMaxLength(127);
+				entity.Property(m => m.NormalizedUserName).HasMaxLength(127);
+            });
+
+            builder.Entity<IdentityRole>(entity => {
+				entity.Property(m => m.Name).HasMaxLength(127);
+				entity.Property(m => m.NormalizedName).HasMaxLength(127);
+            });
+		}
+	}
+}
---
+++ BasicAuthentication/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BasicAuthentication.Models
+{
+    [Table("Items")]
+    public class Item
+    {
+        [Key]
+		public int Id { get; set; }
+		public string Description { get; set; }
+		public virtual ApplicationUser User { get; set; }
+    }
+}
